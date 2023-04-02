8250abe26cac63fabde4682ba45a4de6a17acbb2
Santosh <santosh.kumar.jallepalli@netcracker.com>
2023-04-03 02:02:07 +0530

    Add meta-annotation

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -1,17 +1,9 @@
 package com.example.demo1;
 
-import config.DispatcherServletConfiguration;
-import config.MvcConfiguration;
-import config.TomcatConfiguration;
+import config.MagicApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-@Configuration
-@ComponentScan
-@Import({ TomcatConfiguration.class, DispatcherServletConfiguration.class,
-        MvcConfiguration.class })
+@MagicApplication
 public class Demo1Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
