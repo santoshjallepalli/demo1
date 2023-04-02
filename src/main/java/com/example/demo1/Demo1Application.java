b0ca85f52919e5e7d92eedbc579ce3fe99663356
Santosh <santosh.kumar.jallepalli@netcracker.com>
2023-04-02 23:47:50 +0530

    Extract controller

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -27,16 +25,4 @@ public class Demo1Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
-
-    @RestController
-    class HelloController {
-
-        @RequestMapping("/")
-        public String hello() {
-            return "Hello World";
-        }
-
-    }
-
-
 }
