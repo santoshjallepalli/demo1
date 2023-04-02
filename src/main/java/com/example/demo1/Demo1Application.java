ee50f14fb4324a093372179eca6e14beb9133ece
Santosh <santosh.kumar.jallepalli@netcracker.com>
2023-04-02 20:44:50 +0530

    Initial commit

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -2,8 +2,9 @@ package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 public class Demo1Application {
 
     public static void main(String[] args) {
