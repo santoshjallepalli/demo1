c68690908d3e377bbfaaeab4c96ca308ba40fea1
Santosh <santosh.kumar.jallepalli@netcracker.com>
2023-04-03 00:03:37 +0530

    Extract configuration classes

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -1,29 +1,12 @@
 package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
-@EnableWebMvc
 public class Demo1Application {
-
-    @Bean
-    EmbeddedServletContainerFactory servletContainerFactory() {
-        return new TomcatEmbeddedServletContainerFactory();
-    }
-
-    @Bean
-    DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
