5df5bbb5b8ca4600ddc43a530678a3dc7104f880
Santosh <santosh.kumar.jallepalli@netcracker.com>
2023-04-03 01:19:47 +0530

    Import configuration classes

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -1,11 +1,17 @@
 package com.example.demo1;
 
+import config.DispatcherServletConfiguration;
+import config.MvcConfiguration;
+import config.TomcatConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan
+@Import({ TomcatConfiguration.class, DispatcherServletConfiguration.class,
+        MvcConfiguration.class })
 public class Demo1Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
